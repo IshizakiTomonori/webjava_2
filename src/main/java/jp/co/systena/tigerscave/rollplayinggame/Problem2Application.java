de042a81ca7d326cb203306bd5be18514ad0ccaf
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-11-11 20:44:57 +0900

    [add] Initial Commit
    課題2のプロジェクトをコミット

---
+++ src/main/java/jp/co/systena/tigerscave/rollplayinggame/Problem2Application.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.rollplayinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Problem2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Problem2Application.class, args);
+	}
+
+}
