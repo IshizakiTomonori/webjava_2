5b15a6d2ee6c22af772b96d896726e458e6f319b
ishizakit <ishizakit@DESKTOP-7R4CSIA>
2019-12-06 09:29:34 +0900

    [update] PC返却のため一時退避
    作業の途中だが、PCを返却するために一旦コミット
    
    ・キャラクター選択の作成
    ・コマンド選択画面の作成
    ・Java側の処理は未実装

--- src/main/java/jp/co/systena/tigerscave/rollplayinggame/application/controller/DonburiQuestController.java
+++ src/main/java/jp/co/systena/tigerscave/rollplayinggame/application/controller/DonburiQuestController.java
@@ -2,7 +2,9 @@ package jp.co.systena.tigerscave.rollplayinggame.application.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import jp.co.systena.tigerscave.rollplayinggame.application.service.CharForm;
 
 @Controller
 public class DonburiQuestController {
@@ -10,8 +12,16 @@ public class DonburiQuestController {
   // キャラクター作成画面
   @RequestMapping(value = "/Charcter")
   public ModelAndView CharacterCreate(ModelAndView mav) {
-    mav.addObject("TEXT", "Hello World!");
+//    mav.addObject("TEXT", "Hello World!");
     mav.setViewName("CharacterCreate");
+    return mav;
+  }
+
+  //コマンド画面
+  @RequestMapping(value = "/Command", method = RequestMethod.POST)
+  public ModelAndView Command(ModelAndView mav, CharForm charForm) {
+    mav.addObject("TEXT", "Hello World!");
+    mav.setViewName("Command");
     return mav;
   }
 }
---
+++ src/main/java/jp/co/systena/tigerscave/rollplayinggame/application/service/CharForm.java
@@ -0,0 +1,23 @@
+package jp.co.systena.tigerscave.rollplayinggame.application.service;
+
+public class CharForm {
+  private String charJob;
+  private String charName;
+
+  public String getCharJob(){
+    return this.charJob;
+  }
+
+  public void setCharJob(String ch) {
+    this.charJob = ch;
+  }
+
+  public String getCharName(){
+    return this.charName;
+  }
+
+  public void setCharName(String ch) {
+    this.charName = ch;
+  }
+
+}
